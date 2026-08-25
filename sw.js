038abe615033e5dd1c64df41031c3761cca3a218
habibmosavixyz-commits <habib.mosavi.xyz@gmail.com>
2026-08-25 14:51:37 +0330

    Upgrade service worker cache and fetch strategy
    Updated service worker to use a new cache version and improve fetch handling.

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-const CACHE = 'smoke-counter-v1';
+const CACHE = 'smoke-counter-v2';
+
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./index.html'])));
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+/* همیشه اول از اینترنت میگیره تا آخرین نسخه باشه؛ فقط اگه آفلاین بود از حافظه گوشی میخونه */
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
